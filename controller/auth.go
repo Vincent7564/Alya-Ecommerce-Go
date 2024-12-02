a89cf90027341818330815926e8a6522d187f396
Vin Cent <vin.cent@binus.ac.id>
2024-12-02 21:15:11 +0700

    feat: fix check forgot token bug

--- controller/auth.go
+++ controller/auth.go
@@ -205,18 +205,14 @@ func (c *Controller) CheckForgotPasswordToken(ctx *fiber.Ctx) error {
 		return util.GenerateResponse(ctx, http.StatusBadGateway, "Validation Error", errorMessage)
 	}
 
-	count, err := c.Client.From("reset_password_token").Select("*", "", false).Eq("reset_password_token", request.Token).Single().ExecuteTo(&getData)
+	count, err := c.Client.From("reset_password_tokens").Select("*", "", false).Eq("reset_password_token", request.Token).Single().ExecuteTo(&getData)
 
-	if err != nil {
+	if err != nil && count == 0 {
 		return util.GenerateResponse(ctx, http.StatusInternalServerError, "Internal Server Error", err.Error())
 	}
 
-	if count == 0 {
-		return util.GenerateResponse(ctx, http.StatusBadGateway, "No Token Found", count)
-	}
-
 	if getData.ExpiredAt.Before(time.Now()) {
-		return util.GenerateResponse(ctx, http.StatusUnauthorized, "Token Expired", nil)
+		return util.GenerateResponse(ctx, http.StatusUnauthorized, "Token Expired", "")
 	}
 
 	return util.GenerateResponse(ctx, http.StatusOK, "Token Active", nil)
--- model/entity/reset_password_token.go
+++ model/entity/reset_password_token.go
@@ -3,9 +3,9 @@ package entity
 import "time"
 
 type ResetPasswordToken struct {
-	Username           string    `json:"username"`
-	Email              string    `json:"email"`
-	ResetPasswordToken string    `json:"reset_password_token"`
+	Username           string    `json:"username" validate:"omitempty"`
+	Email              string    `json:"email" validate:"required"`
+	ResetPasswordToken string    `json:"reset_password_token" validate:"required"`
 	CreatedAt          time.Time `json:"created_at"`
 	ExpiredAt          time.Time `json:"expired_at"`
 }
