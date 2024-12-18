6fd5459504cff263fb62cb2ecb88482440356c5a
laurensiavee <92752405+laurensiavee@users.noreply.github.com>
2024-12-18 21:36:09 +0700

    fix: change product route order

--- router/productRoutes.go
+++ router/productRoutes.go
@@ -10,15 +10,15 @@ import (
 func ProductRoutes(app *fiber.App, controller controller.Controller) {
 	ProductRoutes := app.Group("/product", middleware.ValidateTokenMiddleware(controller.Client))
 
-	ProductRoutes.Post("", controller.AddProduct)
-	ProductRoutes.Get("", controller.GetProduct)
-	ProductRoutes.Delete("/:id", controller.DeleteProduct)
-	ProductRoutes.Patch("/:id", controller.UpdateProduct)
-	ProductRoutes.Get("/:id", controller.GetProductById)
-
 	ProductRoutes.Post("/category/", controller.AddCategory)
 	ProductRoutes.Get("/category/", controller.GetCategory)
 	ProductRoutes.Get("/category/:id", controller.GetCategoryById)
 	ProductRoutes.Patch("/category/:id", controller.UpdateCategory)
 	ProductRoutes.Delete("/category/:id", controller.DeleteCategory)
+
+	ProductRoutes.Post("", controller.AddProduct)
+	ProductRoutes.Get("", controller.GetProduct)
+	ProductRoutes.Delete("/:id", controller.DeleteProduct)
+	ProductRoutes.Patch("/:id", controller.UpdateProduct)
+	ProductRoutes.Get("/:id", controller.GetProductById)
 }
