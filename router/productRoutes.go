7ef700996f84a47a8851da3a48ae961cf6f528b5
Vincent <Vin.cent@binus.ac.id>
2025-02-11 13:48:28 +0700

    feat:remove comment

--- router/productRoutes.go
+++ router/productRoutes.go
@@ -21,14 +21,14 @@ func ProductRoutes(app *fiber.App, controller controller.Controller) {
 	ProductRoutes := app.Group("/product", middleware.ValidateTokenMiddleware(controller.Client))
 
 	ProductRoutes.Post("/category/", controller.AddCategory)
-	// ProductRoutes.Get("/category/", controller.GetCategory) // move to public
+	// ProductRoutes.Get("/category/", controller.GetCategory)
 	ProductRoutes.Get("/category/:id", controller.GetCategoryById)
 	ProductRoutes.Patch("/category/:id", controller.UpdateCategory)
 	ProductRoutes.Delete("/category/:id", controller.DeleteCategory)
 
 	ProductRoutes.Post("", controller.AddProduct)
-	// ProductRoutes.Get("", controller.GetProduct) // move to public
+	// ProductRoutes.Get("", controller.GetProduct)
 	ProductRoutes.Delete("/:id", controller.DeleteProduct)
 	ProductRoutes.Patch("/:id", controller.UpdateProduct)
-	// ProductRoutes.Get("/:id", controller.GetProductById) // move to public
+	// ProductRoutes.Get("/:id", controller.GetProductById)
 }
